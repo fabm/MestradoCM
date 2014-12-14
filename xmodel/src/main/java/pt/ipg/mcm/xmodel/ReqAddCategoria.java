ed1c958efa78e0d15902aedcc8e9c7e89b48e8ee
fabm <francisco.ab.monteiro@gmail.com>
2014-12-14 12:45:03 +0000

    add ws categoria

---
+++ xmodel/src/main/java/pt/ipg/mcm/xmodel/ReqAddCategoria.java
@@ -0,0 +1,90 @@
+
+package pt.ipg.mcm.xmodel;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for req-add-categoria complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="req-add-categoria">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="nome" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="descricao" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "req-add-categoria", propOrder = {
+    "nome",
+    "descricao"
+})
+public class ReqAddCategoria {
+
+    @XmlElement(required = true)
+    protected String nome;
+    @XmlElement(required = true)
+    protected String descricao;
+
+    /**
+     * Gets the value of the nome property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * Sets the value of the nome property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNome(String value) {
+        this.nome = value;
+    }
+
+    /**
+     * Gets the value of the descricao property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * Sets the value of the descricao property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDescricao(String value) {
+        this.descricao = value;
+    }
+
+}
