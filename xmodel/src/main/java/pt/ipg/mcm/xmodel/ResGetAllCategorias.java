508e76055a1f6f5bc884a66fc12507640e9e7fe9
fabm <francisco.ab.monteiro@gmail.com>
2015-01-09 17:26:50 +0000

    add a wrapper to categorias list

--- xmodel/src/main/java/pt/ipg/mcm/xmodel/ResGetAllCategorias.java
+++ xmodel/src/main/java/pt/ipg/mcm/xmodel/ResGetAllCategorias.java
@@ -3,6 +3,7 @@ package pt.ipg.mcm.xmodel;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlType;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +12,8 @@ import java.util.List;
 @XmlType(name = "Res-get-all-categorias")
 public class ResGetAllCategorias {
 
-  @XmlElement(required = false)
+  @XmlElementWrapper(name = "categorias",required = true)
+  @XmlElement(name = "categoria",required = false)
   private List<Categoria> categorias;
 
   @XmlElement(required = true)
